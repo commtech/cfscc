bcefd4af2e2b58bf13e93e387e656e223f15cca7
William Fagan <libwilliam@gmail.com>
2013-08-29 19:48:48 -0500

    Update append-status.c

--- examples/append-status/append-status.c
+++ examples/append-status/append-status.c
@@ -1,28 +1,19 @@
-#include <stdlib.h>
-#include <fscc.h>
+#include <stdlib.h> /* EXIT_SUCCESS */
+#include <fscc.h> /* fscc_connect, fscc_disconnect, fscc_handle
+                     fscc_{enable, disable, get}_append_status */
 
 int main(void)
 {
 	fscc_handle h;
-	/*! [Setup variables] */
-	/* Declare our append status variable */
 	unsigned append_status;
-	/*! [Setup variables] */
 
 	fscc_connect(0, 0, &h);
 
-	/*! [Get append status] */
-    /* Get append status value */
 	fscc_get_append_status(h, &append_status);
-	/*! [Get append status] */
 
-	/*! [Enable append status] */
 	fscc_enable_append_status(h);
-	/*! [Enable append status] */
 
-	/*! [Disable append status] */
 	fscc_disable_append_status(h);
-	/*! [Disable append status] */
 
 	fscc_disconnect(h);
 
