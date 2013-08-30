c0be79ffbe75df9b7f853c4b3c4bc759842d8b5a
William Fagan <libwilliam@gmail.com>
2013-08-29 19:56:04 -0500

    Update purge.c

--- examples/purge/purge.c
+++ examples/purge/purge.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
-#include <fscc.h>
+#include <stdlib.h> /* EXIT_SUCCESS */
+#include <fscc.h> /* fscc_connect, fscc_disconnect, fscc_handle
+                     fscc_purge */
 
 int main(void)
 {
@@ -7,20 +8,14 @@ int main(void)
 
 	fscc_connect(0, 0, &h);
 
-	/*! [Purge TX] */
 	/* Purge TX */
 	fscc_purge(h, 1, 0);
-	/*! [Purge TX] */
 
-	/*! [Purge RX] */
 	/* Purge RX */
 	fscc_purge(h, 0, 1);
-	/*! [Purge RX] */
 
-	/*! [Purge both TX & RX] */
 	/* Purge both TX & RX */
 	fscc_purge(h, 1, 1);
-	/*! [Purge both TX & RX] */
 
 	fscc_disconnect(h);
 
